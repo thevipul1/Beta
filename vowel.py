62b62c80cfded89e55b9e034fdef6b26350d768a
thevipul1 <113669501+thevipul1@users.noreply.github.com>
2022-09-16 13:32:50 +0530

    Add files via upload

---
+++ vowel.py
@@ -0,0 +1,8 @@
+#WAP to check if the given alphabet is vowel or consonant.
+
+ch = input("Enter a character: ")
+
+if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u'):
+    print(ch,"is a vowel")
+else:
+    print(ch,"is a consonant")
